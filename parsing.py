efd10dadfee0cac5731331a35c4eb759e564127f
Виктор <maxim.1989Viktor@yandex.ru>
2024-10-22 23:39:24 +0300

    update3

--- parsing.py
+++ parsing.py
@@ -2,14 +2,14 @@ import cianparser
 from time import sleep
 
 a = 0
-while a < 108:
+while a < 54:
     moscow_parser = cianparser.CianParser(location="Москва")
     data = moscow_parser.get_flats(
         deal_type="sale",
-        rooms=(1, 2),
+        rooms=(1),
         with_saving_csv=True,
-        additional_settings={"start_page": 33 + a, "end_page": 34 + a},
+        additional_settings={"start_page": 1 + a, "end_page": 2 + a},
         with_extra_data=True,
     )
-    sleep(120)
+    sleep(60)
     a += 2
