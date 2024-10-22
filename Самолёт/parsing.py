d405084f052421f6777855bcb5fd8fd64da3059f
Виктор <maxim.1989Viktor@yandex.ru>
2024-10-22 22:16:48 +0300

    %%%%

---
+++ Самолёт/parsing.py
@@ -0,0 +1,15 @@
+import cianparser
+from time import sleep
+
+a = 0
+while a < 52:
+    moscow_parser = cianparser.CianParser(location="Москва")
+    data = moscow_parser.get_flats(
+        deal_type="sale",
+        rooms=(1, 2),
+        with_saving_csv=True,
+        additional_settings={"start_page": 33 + a, "end_page": 34 + a},
+        with_extra_data=True,
+    )
+    sleep(120)
+    a += 2
