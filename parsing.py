d3d997adee514a2695746e6de52b46c3709b1081
Виктор <maxim.1989Viktor@yandex.ru>
2024-10-25 11:49:47 +0300

    add several town

--- parsing.py
+++ parsing.py
@@ -2,11 +2,11 @@ import cianparser
 from time import sleep
 
 a = 0
-while a < 54:
-    moscow_parser = cianparser.CianParser(location="Москва")
+while a < 12:
+    moscow_parser = cianparser.CianParser(location="Дубна")
     data = moscow_parser.get_flats(
         deal_type="sale",
-        rooms=(1),
+        rooms=("all"),
         with_saving_csv=True,
         additional_settings={"start_page": 1 + a, "end_page": 2 + a},
         with_extra_data=True,
