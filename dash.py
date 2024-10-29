62d14ae6baa382f0a8f9eb5b24e215374750bc9f
Виктор <maxim.1989Viktor@yandex.ru>
2024-10-30 01:26:00 +0300

    last_upd

--- dash.py
+++ dash.py
@@ -4,36 +4,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 
-df = pd.read_csv("upd4_main.csv", index_col=0)
-list_of_cities = df["location"].unique()
-# print(list_of_cities)
+df = pd.read_csv("my_df.csv", index_col=0)
+# list_of_cities = df["location"].unique()
+# # print(list_of_cities)
 
 
-# Function for calculate price for meter^2
+# # Function for calculate price for meter^2
+dict_city = df['location'].unique()
+
 def price_for_meter(location):
-    city = df[df["location"] == location]
-    city_price = city["price"].sum()
+    city = df[df['location'] == location]
+    price_for_city = city['price'].sum()
+    
+    clean_data = city['total_meters'].sum()
+    
+    return round(price_for_city/clean_data, 2)
 
-    cleaned_data = [x.replace("м²", "").strip() for x in city["total_meters"]]
-    cleaned_data = [x.replace("-1", "0").strip() for x in cleaned_data]
-    cleaned_data = [x.replace("���", "0").strip() for x in cleaned_data]
-    cleaned_data = pd.to_numeric(
-        [x.replace(",", ".").strip() for x in cleaned_data]
-    ).sum()
-
-    return round(city_price / cleaned_data, 2)
-
-
-with open("df_city_meters.csv", "w", newline="", encoding="UTF-8") as csvfile:
-    fieldnames = ["city", "price_for_meter"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+with open('info.csv', 'w', newline='', encoding='UTF-8') as csvfile:
+    names = ['city', 'price_for_meter']
+    writer = csv.DictWriter(csvfile, fieldnames=names)
     writer.writeheader()
-    for city in list_of_cities:
-        writer.writerow({"city": city, "price_for_meter": price_for_meter(city)})
+    for city in dict_city:
+        writer.writerow({'city': city, 'price_for_meter': price_for_meter(city)})
 
-dash_info = pd.read_csv("df_city_meters.csv", index_col=0)
-dash_info.sort_values(["price_for_meter"], axis=0, ascending=[False], inplace=True)
-sns.set_style("whitegrid")
-sns.barplot(x="city", y="price_for_meter", data=dash_info, palette="dark:pink")
-plt.xticks(rotation=75)
-plt.show()
+# dash_info = pd.read_csv("df_city_meters.csv", index_col=0)
+# dash_info.sort_values(["price_for_meter"], axis=0, ascending=[False], inplace=True)
+# sns.set_style("whitegrid")
+# sns.barplot(x="city", y="price_for_meter", data=dash_info, palette="dark:pink")
+# plt.xticks(rotation=75)
+# plt.show()
--- drop.py
+++ drop.py
@@ -33,4 +33,25 @@ df = pd.read_csv("main3.csv")
 # def cleaner(location):
 #     city = df[df["location"] == location]
 
-print(df.shape)
+# print(df.shape)
+# df = df.dropna(subset=['floors_count', 'floor', 'rooms_count'])
+# df["floors_count"] = df["floors_count"].astype(int)
+# df["floor"] = df["floor"].astype(int)
+# df["rooms_count"] = df["rooms_count"].astype(int)
+
+# df["kitchen_meters"] = (df["kitchen_meters"].str.replace("\xa0м²", "").str.replace(",", ".").astype(float))
+# df['living_meters'] = df['living_meters'].str.replace('\xa0м²', '').str.replace(',', '.').astype(float)
+# df.to_csv("main4.csv", index=False)
+list_of_cities = df['location'].unique()
+
+def price_for_meter(location):
+    city = df[df['location']==location]
+    city_price = city['price'].sum()
+    cleaned_data = city['total_meters'].sum()
+    return round(city_price/cleaned_data,2)
+with open("dash_info_fourth.csv", 'w', newline='', encoding='UTF-8') as csvfile:
+    fieldnames = ['city', 'price_for_meter']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for city in list_of_cities:
+        writer.writerow({'city': city, 'price_for_meter': price_for_meter(city)})
