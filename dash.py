4e49871ddc5327203c78f7f6a51b5a22ca570265
Виктор <maxim.1989Viktor@yandex.ru>
2024-10-30 01:50:39 +0300

    upd7

--- dash.py
+++ dash.py
@@ -1,35 +1,21 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 import csv
+import warnings
+warnings.simplefilter('ignore')
 
 df = pd.read_csv("my_df.csv", index_col=0)
-# list_of_cities = df["location"].unique()
-# # print(list_of_cities)
 
-
-# # Function for calculate price for meter^2
-dict_city = df['location'].unique()
-
-def price_for_meter(location):
-    city = df[df['location'] == location]
-    price_for_city = city['price'].sum()
-    
-    clean_data = city['total_meters'].sum()
-    
-    return round(price_for_city/clean_data, 2)
-
-with open('info.csv', 'w', newline='', encoding='UTF-8') as csvfile:
-    names = ['city', 'price_for_meter']
-    writer = csv.DictWriter(csvfile, fieldnames=names)
-    writer.writeheader()
-    for city in dict_city:
-        writer.writerow({'city': city, 'price_for_meter': price_for_meter(city)})
-
-# dash_info = pd.read_csv("df_city_meters.csv", index_col=0)
-# dash_info.sort_values(["price_for_meter"], axis=0, ascending=[False], inplace=True)
-# sns.set_style("whitegrid")
-# sns.barplot(x="city", y="price_for_meter", data=dash_info, palette="dark:pink")
-# plt.xticks(rotation=75)
-# plt.show()
+df = df.replace(-1,np.nan)
+df = df.replace("-1",np.nan)
+df = df.replace(-1.0,np.nan)
+df = df.replace("-1.0",np.nan)
+df = df.dropna(subset=['location', 'price', 'rooms_count'])
+df['floor'] = df['floor'].astype(int)
+df['floors_count'] = df['floors_count'].astype(int)
+df['rooms_count'] = df['rooms_count'].astype(int)
+df.drop(['phone', 'deal_type', 'accommodation_type', 'object_type', 'heating_type'], axis=1, inplace=True)
+print(f'Количество столбцов после чистки {df.shape[1]} столбцы')
+# Необходимо в сохранённом файле изменить ��� на 0, для избежания ошибок
